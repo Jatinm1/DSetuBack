b89d847d6d4e5d3b0d9568d603de51f23b983e1d
JatinMalik <jatinmalikgit@gmail.com>
2025-04-22 15:16:40 +0530

    checking

--- DealerSetu_Data/Models/HelperModels/ETSContext.cs
+++ DealerSetu_Data/Models/HelperModels/ETSContext.cs
@@ -11,7 +11,6 @@ namespace DealerSetu_Data.Models.HelperModels
 
         public DbSet<UserDetails> tb_userDetails { get; set; }
 
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<UserDetails>(entity =>
